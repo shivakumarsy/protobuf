f4415b5963bde90d7800bbb52f0e5c89fa8576b0
Shivakumar s poojari <shivukumar076@gmail.com>
2020-12-18 23:32:16 +0530

    committin all

---
+++ src/main/java/com/supernet/protobuf/PersonDemo.java
@@ -0,0 +1,14 @@
+package com.supernet.protobuf;
+
+import com.supernet.models.Person;
+
+public class PersonDemo {
+
+    public static void main(String[] args) {
+//        Person test = Person.newBuilder()
+//        .setAge(10)
+//        .setName("test")
+//        .build();
+//        System.out.println(test.toString());
+    }
+}
